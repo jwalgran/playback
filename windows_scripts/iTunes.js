6db6cd0fc0baf5e6e5ca881e08551d70c9727b33
Carlos Rosquillas <crosquillas@gmail.com>
2015-07-07 22:26:06 -0500

    Return loved status in json
    Fix indentation (+2 squashed commits)
    Squashed commits:
    [8478a0f] Return as boolean on osx
    [2a6400d] Return loved status in json

--- windows_scripts/iTunes.js
+++ windows_scripts/iTunes.js
@@ -22,7 +22,8 @@ var getCurrentTrack = function() {
         return JSON.stringify({
             name: currentTrack.Name,
             artist: currentTrack.Artist,
-            album: currentTrack.Album
+            album: currentTrack.Album,
+            loved: currentTrack.Loved
         });
     } else {
         return null;
